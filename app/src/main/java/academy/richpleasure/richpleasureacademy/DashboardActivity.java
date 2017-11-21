6dc2b5c32d41e3c53ed7b86f0580d3a5369b4867
Imran <imranhamzah@gmail.com>
2017-11-21 10:06:01 +0800

    Add Local notification and slider at Dashboard

--- app/src/main/java/academy/richpleasure/richpleasureacademy/DashboardActivity.java
+++ app/src/main/java/academy/richpleasure/richpleasureacademy/DashboardActivity.java
@@ -1,13 +1,31 @@
 package academy.richpleasure.richpleasureacademy;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
 public class DashboardActivity extends AppCompatActivity {
 
+
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] XMEN= {R.drawable.beast,R.drawable.charles,R.drawable.magneto,R.drawable.mystique,R.drawable.wolverine};
+    private ArrayList<Integer> XMENArray = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +33,18 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_dashboard);
+        init();
 
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent("android.media.action.DISPLAY_NOTIFICATION");
+        notificationIntent.addCategory("android.intent.category.DEFAULT");
+
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 15);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
     }
 
     public void gotoSeminar(View view)
@@ -27,5 +56,33 @@ public class DashboardActivity extends AppCompatActivity {
     public void gotoSubjects(View view) {
         Intent intent = new Intent(this,SubjectsActivity.class);
         startActivity(intent);
+    }
+
+    private void init() {
+        for(int i=0;i<XMEN.length;i++)
+            XMENArray.add(XMEN[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new SlideAdapter(DashboardActivity.this,XMENArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == XMEN.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 4000, 4000);
     }
 }
---
+++ app/src/main/java/academy/richpleasure/richpleasureacademy/NotificationActivity.java
@@ -0,0 +1,13 @@
+package academy.richpleasure.richpleasureacademy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
