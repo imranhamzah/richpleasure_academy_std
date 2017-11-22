8942bd17408d6b12297e22760048890915a17dec
Imran <imranhamzah@gmail.com>
2017-11-22 13:04:47 +0800

    Update code with PlaceViewHolder

---
+++ app/src/main/java/academy/richpleasure/richpleasureacademy/DashboardDrawerMenuItem.java
@@ -0,0 +1,8 @@
+package academy.richpleasure.richpleasureacademy;
+
+/**
+ * Created by Imran on 11/21/2017.
+ */
+
+public class DashboardDrawerMenuItem {
+}
--- app/src/main/java/academy/richpleasure/richpleasureacademy/SubjectsActivity.java
+++ app/src/main/java/academy/richpleasure/richpleasureacademy/SubjectsActivity.java
@@ -3,6 +3,7 @@ package academy.richpleasure.richpleasureacademy;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +13,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.mindorks.placeholderview.annotations.Click;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
 
 public class SubjectsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    SwipePlaceHolderView mCardsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
+
+
+
+
+        mCardsContainer = (SwipePlaceHolderView) findViewById(R.id.cards_container);
+
+        mCardsContainer.getBuilder()
+//                .setDisplayViewCount(1)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f));
+
+        mCardsContainer.addView(new CardPresenter("QUESTION1"));
+        mCardsContainer.addView(new CardPresenter("QUESTION2"));
+        mCardsContainer.addView(new CardPresenter("QUESTION3"));
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +70,54 @@ public class SubjectsActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @NonReusable
+    @Layout(R.layout.card_layout)
+    public class CardPresenter {
+        @com.mindorks.placeholderview.annotations.View(R.id.question_caption)
+        TextView mTextView;
+
+        String question;
+
+        public CardPresenter(String question) {
+            this.question = question;
+        }
+
+        @Click(R.id.btn_answer1)
+        public void onClickQ1() {
+            String message = String.format(
+                    "The question is: %s. The answer is: %s.",
+                    this.question,
+                    "ANSWER1");
+            Log.i("PlaceholderView", message);
+            mTextView.setText(message);
+        }
+
+        @Click(R.id.btn_answer2)
+        public void onClickQ2() {
+            String message = String.format(
+                    "The question is: %s. The answer is: %s.",
+                    this.question,
+                    "ANSWER2");
+            Log.i("PlaceholderView", message);
+            mTextView.setText(message);
+        }
+
+        @Click(R.id.btn_answer3)
+        public void onClickQ3() {
+            String message = String.format(
+                    "The question is: %s. The answer is: %s.",
+                    this.question,
+                    "ANSWER3");
+            Log.i("PlaceholderView", message);
+            mTextView.setText(message);
+        }
+
+        @Resolve
+        private void onResolved(){
+            mTextView.setText(this.question);
+        }
     }
 
     @Override
